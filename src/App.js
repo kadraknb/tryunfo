d97974537dbbf2a071c9c60c17f4c68cf170cb3b
kadraknb <kadrak02@hotmail.com>
2022-06-29 02:07:12 -0300

    [vagner] iniciando o projeto.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return (
-      <div>
-        <h1>Tryunfo</h1>
-      </div>
+    return (<div><h1>Tryunfo</h1></div>
     );
   }
 }
